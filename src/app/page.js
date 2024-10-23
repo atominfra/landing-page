ccc77938f2a9867625e551058448c61e43821f9a
Shryansh <112005671+Shryansh107@users.noreply.github.com>
2024-10-23 11:04:42 +0530

    fix: centered bttn on product card (#4)

--- src/app/page.js
+++ src/app/page.js
@@ -51,8 +51,8 @@ export default function Home() {
         Seamless AI development, deployment and monitoring<br />
         in Cloud all through one interface!
       </p>
-      <a href={`${process.env.NEXT_AICLOUDLAB_URL}`} target='_blank'>
-      <button className="inline-block bg-[#009ded] text-white px-4 py-2 w-[150px] rounded-full hover:bg-[#3591c0] transition-colors flex justify-center items-center " >
+      <a href={`${process.env.NEXT_AICLOUDLAB_URL}`} target='_blank' className='flex  justify-center'>
+      <button className="inline-block bg-[#009ded]  text-white px-4 py-2 w-[150px] rounded-full hover:bg-[#3591c0] transition-colors flex justify-center items-center " >
         <div className='flex gap-1 font-bold'>Visit <GoLinkExternal className=' font-bold stroke-2 mt-1'/></div>
       </button>
         </a>
