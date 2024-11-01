c35726abc34c631ed9efef6b18c7565ab857faff
Shryansh <112005671+Shryansh107@users.noreply.github.com>
2024-11-01 22:17:47 +0530

    feat: adds features section (#12)

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import footerBackgroundImage from '@/assets/layered-waves-footer.svg';
 import Image from 'next/image';
 import ScrollToTopButton from '@/components/ScrollToTopButton';
 import { GoLinkExternal } from "react-icons/go";
+import data from "@/utils/teamData"
 
 export default function Home() {
   const { teamMembers, advisors } = teamData;
@@ -37,25 +38,47 @@ export default function Home() {
   className="w-full min-h-screen flex flex-col items-center justify-center max-w-6xl mx-auto py-12 px-4"
 >
   <h2 className="text-4xl font-bold text-center mb-20 text-gray-500 mt-28 lg:mt-0">Our Products</h2>
-  <div className="bg-gray-900 p-6 rounded-lg border-4 border-[#007be5] h-auto md:h-[40vh] w-[80vw] md:w-[50vw] flex flex-col md:flex-row  items-center justify-evenly transition-all">
+  <div className="p-[4px] rounded-lg bg-gradient-to-r from-[#0EB3FF] to-[#FFFFFF] w-[90vw] md:w-[85vw]">
+  <div className="bg-gray-900 p-3 md:p-6 rounded-lg h-auto transition-all">
+  
+  <div className='flex flex-col md:flex-row  items-center justify-evenly '>
     <Image
-      src="https://res.cloudinary.com/dy8hx2xrj/image/upload/v1729417887/New_Project_mdpjqy.png"
+      src="https://res.cloudinary.com/dy8hx2xrj/image/upload/v1730454166/Frame_1_e7wgpb.png"
       alt="AI cloud lab"
-      height={1000}
-      width={1000}
-      className="object-contain h-[14rem]  md:h-[18rem] md:w-[10rem]"
+      height={300}
+      width={400}
+      className="object-contain h-[14rem]  md:h-[18rem] md:w-[25rem]"
     />
-    <div className="flex flex-col justify-center text-center md:text-left">
+    <div className="flex flex-col justify-center text-center md:text-left md:w-[35vw]">
       <h3 className="text-2xl font-semibold mb-4">AI Cloud Lab</h3>
       <p className="text-white mb-6">
         Seamless AI development, deployment and monitoring<br />
         in Cloud all through one interface!
       </p>
       <a href={`${process.env.NEXT_AICLOUDLAB_URL}`} target='_blank' className='flex md:block  justify-center'>
-      <button className="inline-block bg-[#009ded]  text-white px-4 py-2 w-[150px] rounded-full hover:bg-[#3591c0] transition-colors flex justify-center items-center " >
+      <button className=" bg-[#009ded]  text-white px-4 py-2 w-[150px] rounded-full hover:bg-[#3591c0] transition-colors flex justify-center items-center " >
         <div className='flex gap-1 font-bold'>Visit <GoLinkExternal className=' font-bold stroke-2 mt-1'/></div>
       </button>
         </a>
+      </div>
+        </div>
+    <div className='text-center w-full font-poppins text-3xl font-semibold my-20'>We make AI deployment <span className="italic bg-gradient-to-r from-[#1AB7FF] to-[#0051FF] bg-clip-text text-transparent">
+  super-fast 
+</span> {" "}
+ and affordable!</div>
+ <div className="flex flex-wrap justify-center">
+      {data.aicloudlab_features.map((box, index) => (
+         <div key={index} className="w-full  md:w-[40%]  border-2 rounded-[10px] p-8 font-poppins bg-gray-800 m-4 md:min-h-[200px]">
+         <div className="mb-2 text-2xl font-semibold">{box.title}</div>
+         <div>{box.description}</div>
+       </div>
+      ))}
+    </div>
+    </div>
+  <div>
+  
+ 
+
     </div>
   </div>
 </section>
