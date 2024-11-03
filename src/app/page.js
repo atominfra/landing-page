d4ee705de5a3d0e86815698f839db6ae81e9aeb7
shryansh <shryansh2024@gmail.com>
2024-11-03 11:16:44 +0530

    fixes bottom margin in visit button

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
         in Cloud all through one interface!
       </p>
       <a href={`${process.env.NEXT_AICLOUDLAB_URL}`} target='_blank' className='flex lg:block  justify-center'>
-      <button className=" bg-[#009ded]  text-white px-4 py-2 w-[150px] rounded-full hover:bg-[#3591c0] transition-colors flex justify-center items-center " >
+      <button className=" bg-[#009ded]  text-white px-4 mb-6 py-2 w-[150px] rounded-full hover:bg-[#3591c0] transition-colors flex justify-center items-center " >
         <div className='flex gap-1 font-bold'>Visit <GoLinkExternal className=' font-bold stroke-2 mt-1'/></div>
       </button>
         </a>
