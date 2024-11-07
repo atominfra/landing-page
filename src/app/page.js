6baaba5b370d0535af0b05bfe8988eebd3c9399e
Prabhmeet Singh <98380208+Prabhmeetchef@users.noreply.github.com>
2024-11-07 20:05:54 +0530

    imroved spacing

--- src/app/page.js
+++ src/app/page.js
@@ -130,12 +130,12 @@ export default function Home() {
       <section className="w-full h-[25vh] relative overflow-hidden flex">
         <div className="absolute inset-0 border-t-2 border-gray-600">
           <h1 className='md:text-lg text-[15px] top-[5vh] left-[4.7vw] relative md:top-[6vh] md:left-[6vw]'><b>Ambition Forge Pvt. Ltd</b></h1>
-          <h2 className='relative top-[5.2vh] left-[4.7vw] md:left-[6vw] md:text-[1rem] text-[12px]'> Indiranagar, Bangalore<br/>
+          <h2 className='relative top-[5.2vh] left-[4.7vw] md:left-[6vw] md:top-[7.2vh] md:text-[1rem] text-[12px]'> Indiranagar, Bangalore<br/>
           560038
           </h2>
-          <h1 className='absolute text-[15px] md:top-[6vh] md:text-lg md:right-[7.2vw] right-[56vw] top-[15vh]'>
+          <h1 className='absolute text-[15px] md:top-[6vh] md:text-lg md:right-[7.2vw] md:left-auto left-[4.7vw] top-[15vh]'>
             <span className="block md:inline text-left">Reach out to us: </span>
-            <span className="block md:inline underline"><a href='mailto:hello@atominfra.com'><b>hello@atominfra.com</b></a></span>
+            <span className="block md:inline "><a href='mailto:hello@atominfra.com'>hello@atominfra.com</a></span>
           </h1>
           <div className=' absolute md:top-[12vh] w-[92px] right-[7vw] top-[5.2vh]'>
           <SocialPill/>
