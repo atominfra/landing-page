0aa56c87e9a76d04a08ad3271a60cd705638c94f
shryansh <shryansh2024@gmail.com>
2024-11-07 13:41:34 +0530

    team data correction

--- src/utils/teamData.js
+++ src/utils/teamData.js
@@ -14,7 +14,7 @@ const teamData = {
     },
     {
       name: 'Shryansh',
-      role: 'Founding Member, Full Stack',
+      role: 'Founding Member, Frontend',
       photo: 'https://res.cloudinary.com/dy8hx2xrj/image/upload/v1728836215/shryansh_qfjiae.jpg',
       linkedinUrl: 'https://www.linkedin.com/in/shryansh-chaudhary/',
     },
@@ -26,7 +26,7 @@ const teamData = {
     },
     {
       name: 'Swastik Singh',
-      role: 'Founding Member, Python Developer',
+      role: 'Founding Member, Backend',
       photo: 'https://res.cloudinary.com/dy8hx2xrj/image/upload/v1730966072/swastik_1_cropped_odlm5m.jpg',
       linkedinUrl: 'https://www.linkedin.com/in/swastik-bhowmick-24169b224/#/',
     },
