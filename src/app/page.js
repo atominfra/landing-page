8404ff267b10a09bffed5981fdff94a3a8518323
Prabhmeet Singh <98380208+Prabhmeetchef@users.noreply.github.com>
2024-11-07 17:06:52 +0530

    enhanced mobile responsiveness and fixed scroll button

--- src/app/page.js
+++ src/app/page.js
@@ -129,14 +129,15 @@ export default function Home() {
       {/* Footer Section */}
       <section className="w-full h-[25vh] relative overflow-hidden flex">
         <div className="absolute inset-0 border-t-2 border-gray-600">
-          <h1 className='text-lg relative top-[6vh] left-[6vw]'><b>Ambition Forge</b> Pvt. Ltd</h1>
-          <h2 className='relative top-[7vh] left-[6vw]'> Indiranagar, Bangalore<br/>
+          <h1 className='md:text-lg text-[15px] top-[6vh] left-[4.7vw] relative md:top-[6vh] md:left-[6vw]'><b>Ambition Forge</b> Pvt. Ltd</h1>
+          <h2 className='relative top-[7vh] left-[4.7vw] md:left-[6vw] md:text-[1rem] text-[12px]'> Indiranagar, Bangalore<br/>
           560038
           </h2>
-          <h1 className='absolute top-[6vh] text-lg left-[70vw]'>
-            Reach out to us at <a href='mailto:hello@atominfra.com'><b>hello@atominfra.com</b></a>
+          <h1 className='hidden md:block absolute text-[15px] md:top-[6vh] md:text-lg md:right-[7.2vw] right-[4.7vw] top-[6vh]'>
+            <span className="block md:inline text-right">Reach out to us at: </span>
+            <span className="block md:inline"><a href='mailto:hello@atominfra.com'><b>hello@atominfra.com</b></a></span>
           </h1>
-          <div className='absolute top-[12vh] min-w-[88px] max-w-[6.2vw] right-[7vw]'>
+          <div className=' absolute md:top-[12vh] w-[92px] right-[7vw] top-[6.2vh]'>
           <SocialPill/>
           </div>
         </div>
