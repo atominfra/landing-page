fb97e01e7cdbbe2338f6fb323fe81306c6ac7e85
Shryansh <112005671+Shryansh107@users.noreply.github.com>
2024-12-13 00:10:02 +0530

    merges v0-dev into v0 (#44)
    * Feat/redesign (#41)
    
    * redesigned landing page
    
    * revamped landing apge
    
    * fixes responsiveness of contact us page and navMenu
    
    ---------
    
    Co-authored-by: Shryansh <shryansh2024@gmail..com>
    
    * Feat/redesign (#43)
    
    * redesigned landing page
    
    * revamped landing apge
    
    * fixes responsiveness of contact us page and navMenu
    
    * fixes build errors
    
    ---------
    
    Co-authored-by: Shryansh <shryansh2024@gmail..com>
    
    * renames env var from NEXT_AICLOUDLAB_URL NEXT_PUBLIC_AICLOUDLAB_URL
    
    ---------
    
    Co-authored-by: Shryansh <shryansh2024@gmail..com>

--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -12,7 +12,7 @@ export default function TermsOfService() {
         <h1 className="text-4xl font-bold mb-6 text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-blue-400">Terms & Conditions</h1>
         <p className="mb-8 text-gray-600 dark:text-gray-300 text-center">Last updated on Nov 28th 2024</p>
         <div className="prose dark:prose-invert max-w-none">
-          <p className="mb-4">For the purpose of these Terms and Conditions, The term "we", "us", "our" used anywhere on this page shall mean AMBITION FORGE PRIVATE LIMITED, whose registered/operational office is 235 Binnamangala, 13th Cross Road, 2nd Stage, Indiranagar Bengaluru KARNATAKA 560038 . "you", "your", "user", "visitor" shall mean any natural or legal person who is visiting our website and/or agreed to purchase from us.</p>
+          <p className="mb-4">For the purpose of these Terms and Conditions, The term &quot;we&quot;, &quot;us&quot;, &quot;our&quot; used anywhere on this page shall mean AMBITION FORGE PRIVATE LIMITED, whose registered/operational office is 235 Binnamangala, 13th Cross Road, 2nd Stage, Indiranagar Bengaluru KARNATAKA 560038 . &quot;you&quot;, &quot;your&quot;, &quot;user&quot;, &quot;visitor&quot; shall mean any natural or legal person who is visiting our website and/or agreed to purchase from us.</p>
           
           <p className="mb-4">Your use of the website and/or purchase from us are governed by following Terms and Conditions:</p>
           
@@ -24,12 +24,12 @@ export default function TermsOfService() {
             <li>All trademarks reproduced in our website which are not the property of, or licensed to, the operator are acknowledged on the website.</li>
             <li>Unauthorized use of information provided by us shall give rise to a claim for damages and/or be a criminal offense.</li>
             <li>From time to time our website may also include links to other websites. These links are provided for your convenience to provide further information.</li>
-            <li>You may not create a link to our website from another website or document without AMBITION FORGE PRIVATE LIMITED's prior written consent.</li>
+            <li>You may not create a link to our website from another website or document without AMBITION FORGE PRIVATE LIMITED&apos;s prior written consent.</li>
             <li>Any dispute arising out of use of our website and/or purchase with us and/or any engagement with us is subject to the laws of India.</li>
             <li>We, shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any Transaction, on Account of the Cardholder having exceeded the preset limit mutually agreed by us with our acquiring bank from time to time</li>
           </ul>
           
-          <p className="bg-blue-100 dark:bg-blue-900 p-4 rounded-lg"><strong>Disclaimer:</strong> The above content is created at AMBITION FORGE PRIVATE LIMITED's sole discretion. Razorpay shall not be liable for any content provided here and shall not be responsible for any claims and liability that may arise due to merchant's non-adherence to it.</p>
+          <p className="bg-blue-100 dark:bg-blue-900 p-4 rounded-lg"><strong>Disclaimer:</strong> The above content is created at AMBITION FORGE PRIVATE LIMITED&apos;s sole discretion. Razorpay shall not be liable for any content provided here and shall not be responsible for any claims and liability that may arise due to merchant&apos;s non-adherence to it.</p>
         </div>
       </main>
     </div>
