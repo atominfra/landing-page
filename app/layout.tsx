96765a0793ff1a95bd3e910b4866ea0e7e4fd44a
Shryansh <shryansh2024@gmail.com>
2025-01-03 16:41:28 +0530

    changes metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "AtomInfra",
-  description: "Making AI Accessible",
+  title: "Atom Infra",
+  description: "Democratising Compute - Seamlessly manage deployments across various providers through a consistent and user-friendly interface",
   icons: {
     icon: '/favicon.ico',
   },
