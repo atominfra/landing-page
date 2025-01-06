f26ed7bc60c55dfc7facc77fb540ad9c44a08678
Shryansh <112005671+Shryansh107@users.noreply.github.com>
2025-01-06 20:50:17 +0530

    merges v0-dev into v0 (#57)
    * fixes build errors
    
    * fixes cards responsiveness
    
    * removes aicloudlab branding
    
    * changes atomInfraLogo

--- components/header.tsx
+++ components/header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Github, Linkedin } from "lucide-react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross2 } from "react-icons/rx";
+import Image from "next/image";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -82,9 +83,22 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 flex justify-center border-b bg-gradient-to-b from-white to-blue-50 dark:from-gray-900 dark:to-gray-800 backdrop-blur-sm dark:border-gray-800">
       <div className="container flex h-16 items-center justify-between max-w-7xl">
-        <Link href="/" className="flex items-center space-x-2">
-          <span className="font-bold text-xl text-blue-600 dark:text-blue-400 pl-4">AtomInfra</span>
-        </Link>
+      <div className='flex items-center'>
+          <Image
+            src="https://res.cloudinary.com/dy8hx2xrj/image/upload/v1734099746/atominfra-logo_pmfxxq.png" 
+            width={30}
+            height={30}
+            alt="Atom Infra Logo"
+            className='h-[22px] w-[23px] pt-[1px]'
+            priority
+          />
+          <div 
+            className='relative text-[24px] font-[700] tracking-tight l-[30px]' 
+            style={{ left: '-2px' }}
+          >
+            tom Infra
+          </div>
+        </div>
         <div className="flex items-center lg:hidden pr-4">
           <button
             className="text-blue-600 dark:text-blue-400 focus:outline-none"
