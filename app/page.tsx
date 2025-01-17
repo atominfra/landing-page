c2088b780e69791063df8d599568e0daeebb45b4
Karun Agarwal <singhalkarun@gmail.com>
2025-01-17 13:44:05 +0530

    remove features section

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <HeroSection />
-        <FeaturesSection />
+        {/* <FeaturesSection /> */}
         <PartnersSection />
         <TeamSection />
         <AdvisorsSection />
